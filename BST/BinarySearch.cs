368edd5d39eec3b21406e9c823f30e793c28432f
varun <varunlad5@gmail.com>
2021-09-22 21:20:05 +0530

    [varun] Binary search Tree

---
+++ BST/BinarySearch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST
+{
+    class BinaryTree
+    {
+        public Node Root { get; set; }
+
+        public bool Add(int value)
+        {
+            Node before = null, after = this.Root;
+
+            while (after != null)
+            {
+                before = after;
+                if (value < after.Data) //Is new node in left tree? 
+                    after = after.LeftNode;
+                else if (value > after.Data) //Is new node in right tree?
+                    after = after.RightNode;
+                else
+                {
+                    //Exist same value
+                    return false;
+                }
+            }
+
+            Node newNode = new Node();
+            newNode.Data = value;
+
+            if (this.Root == null)//Tree ise empty
+                this.Root = newNode;
+            else
+            {
+                if (value < before.Data)
+                    before.LeftNode = newNode;
+                else
+                    before.RightNode = newNode;
+            }
+            Console.WriteLine(newNode.Data);
+            return true;
+        }
+
+
+    }
+}
---
+++ BST/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Search Tree");
+            BinaryTree binaryTree = new BinaryTree();
+
+            binaryTree.Add(56);
+            binaryTree.Add(30);
+            binaryTree.Add(70);
+            binaryTree.Add(60);
+            binaryTree.Add(78);
+            binaryTree.Add(80);
+            binaryTree.Add(50);
+            Console.ReadLine();
+        }
+    }
+}
